58cacbde1736194f7122d43e9642a07d96f1204a
Kevin Kwok <antimatter15@gmail.com>
2019-10-10 19:30:42 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,14 @@ getLibraryOptions() {
     done
 }
 
-BUILDER_OPTIONS="-hardware $BUILDER_PATH/packages  $BUILDER_PATH/hardware -tools $BUILDER_PATH/hardware/tools/avr -tools $BUILDER_PATH/tools-builder `getLibraryOptions $LIBRARIES_PATH` -fqbn $BOARD_NAME -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=$BUILDER_PATH/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-3-20ed2b9 -prefs=runtime.tools.python.path=/usr/bin/"
+BUILDER_OPTIONS="-hardware $BUILDER_PATH/packages $BUILDER_PATH/hardware -tools $BUILDER_PATH/hardware/tools/avr -tools $BUILDER_PATH/tools-builder `getLibraryOptions $LIBRARIES_PATH` -fqbn $BOARD_NAME -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=$BUILDER_PATH/packages/esp8266/tools/xtensa-lx106-elf-gcc/2.5.0-3-20ed2b9 -prefs=runtime.tools.python.path=/usr/bin/"
 
 if [ -d "$INPUT_HARDWARE" ]; then
     BUILDER_OPTIONS="$BUILDER_OPTIONS -hardware $INPUT_HARDWARE"
 fi
+
+
+echo $BUILDER_OPTIONS
 
 if [ -n "$SKETCH_PATH" ]; then
     if [ -z "$1" ]; then
